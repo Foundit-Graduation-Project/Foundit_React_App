2a0b98c00094bb98926c6385de84b7d79ce7b08b
Ziad-Abdallah <ziad38107@gmail.com>
2026-02-22 14:54:06 +0200

    Update: store/store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reportReducer from "../features/reports/reportsSlice";
+import chatReducer from './chatSlice';
 export const store = configureStore({
   reducer: {
     report: reportReducer,
+    chat: chatReducer,
   },
 });
+
