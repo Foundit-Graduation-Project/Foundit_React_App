62fce5c7a519141282d40f853435206d1c560ebb
Badr-Mohamed <badrali802@gmail.com>
2026-02-26 17:07:31 +0200

    refactor:minimize font size in mobile UI

--- src/pages/Reports/ReportFooter.jsx
+++ src/pages/Reports/ReportFooter.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ReportFooter() {
   return (
     <footer className="bg-white flex content-center justify-center text-center py-10 lg:block dark:bg-amber-400">
-      <span className="text-slate-500">
+      <span className="text-slate-500 text-sm">
         @2025 Find Hup Community. protected what matters to you
       </span>
     </footer>
