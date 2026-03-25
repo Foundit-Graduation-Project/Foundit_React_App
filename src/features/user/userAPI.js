c9bc5c895549816f9ef586551de9297a996b3258
Badr Mohamed <badrali802@gmial.com>
2026-03-25 22:56:02 +0200

    feat(user): implement user state management (slice, selectors, API)

--- src/features/user/userAPI.js
+++ src/features/user/userAPI.js
@@ -1,1 +1,25 @@
 // User API endpoints
+import api from "../../services/axios";
+
+export const getMeAPI = async () => {
+    const res = await api.get("/users/me");
+    return res.data?.data?.user || res.data?.user || res.data;
+};
+
+export const updateMeAPI = async (data) => {
+    const res = await api.patch("/users/update-me", data);
+    return res.data?.data?.user || res.data?.user || res.data;
+};
+
+export const updateAvatarAPI = async (file) => {
+    const formData = new FormData();
+    formData.append("avatar", file);
+
+    const res = await api.patch("/users/update-avatar", formData, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    });
+
+    return res.data?.data?.user || res.data?.user || res.data;
+};
